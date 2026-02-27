9f751b16f1269f51205ac269300f69e6d5efd34b
Kosi Ezewudo <113574452+ToniKosi@users.noreply.github.com>
2026-02-27 14:36:52 -0600

    Prompt-engineered layers of RAG module + added a general answer node.

--- backend/orchestrator/graph.py
+++ backend/orchestrator/graph.py
@@ -1,6 +1,6 @@
 from langgraph.graph import MessagesState, StateGraph, START, END
 from nodes import retrieve_context
-from nodes import query_clean, query_route, generate_answer
+from nodes import query_clean, query_route, general_answer, rusty_answer
 from nodes2 import metadata_filter_node
 from patent_miner_classes import metadatastate
 from tools import routing_function
@@ -18,7 +18,8 @@ def build_graph():
     workflow.add_node("query_route", query_route)
     workflow.add_node("metadata_filter", metadata_filter_node)
     workflow.add_node("retrieve", retrieve_context)
-    workflow.add_node("generate_answer", generate_answer)
+    workflow.add_node("general_answer", general_answer)
+    workflow.add_node("rusty_answer", rusty_answer)
 
     workflow.add_edge(START, "query_clean")
     workflow.add_edge("query_clean", "query_route")  #NEW
@@ -26,12 +27,13 @@ def build_graph():
     workflow.add_conditional_edges(
     "query_route",
     routing_function,
-    {"metadatafilter": "metadata_filter", "answer": "generate_answer"},
+    {"metadatafilter": "metadata_filter", "answer": "general_answer"},
     )
 
     workflow.add_edge("metadata_filter", "retrieve")
-    workflow.add_edge("retrieve", "generate_answer")   # changed
-    workflow.add_edge("generate_answer", END)
+    workflow.add_edge("retrieve", "rusty_answer")   # changed
+    workflow.add_edge("rusty_answer", END)
+    workflow.add_edge("general_answer", END)
 
     return workflow
 
--- backend/orchestrator/nodes.py
+++ backend/orchestrator/nodes.py
@@ -13,18 +13,21 @@ load_dotenv()
 
 
 nodes_model = ChatOpenAI(
-    model="protected.gemini-2.5-flash",
+    model="protected.gpt-4.1",
     temperature=0.2,
 )
 
-clean_structured_llm = nodes_model.with_structured_output(CleanedQuery)
+#clean_structured_llm = nodes_model.with_structured_output(CleanedQuery)
 
 ## Query Cleaning Node ##
 
 
 clean_prompt = (
-    "Rewrite the user's question into ONE concise search query. "
-    "Return only the cleaned query."
+    "You are a patent search and retrieval assistant. \n"
+    "Given a user question, detect and correct grammatical errors and misspellings. \n"
+    "If the question contains ambiguous or vague words/phrases - rewrite it concisely while preserving meaning. \n"
+    "If the question contains no errors or ambiguity - do not rewite it. \n"
+    "Here is the user question: {question}"
 )
 
 
@@ -34,14 +37,8 @@ def query_clean(state: MessagesState):
     question = messages[0].content
     prompt = clean_prompt.format(question=question)
     
-    obj: CleanedQuery = clean_structured_llm.invoke(
-        [{"role": "system", "content": prompt},
-         {"role": "user", "content": question}]
-    )
-
-    cleaned = obj.cleaned_query.strip()
-    return {"messages": [{"role": "user", "content": cleaned}],
-            "cleaned_query": cleaned}
+    cleaned_q = nodes_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": [cleaned_q],"cleaned_query": cleaned_q}
 
 ## Query Routing Node ##
 
@@ -49,14 +46,14 @@ def query_route(state: MessagesState) -> retrievalstate:
     user_text = state["messages"][-1].content
 
     prompt = (
-        "You are a router for a RAG system.\n"
-        "Decide if answering the user requires retrieving external context.\n\n"
+        "You are a router for a Patent search and retrieval RAG system.\n"
+        f"Given the question: \n{user_text}\n"
+        "Decide if answering the user requires retrieving context from the RAG patent database.\n\n"
         "Return ONLY valid JSON with exactly this schema:\n"
         '{"needs_retrieval": true|false}\n\n'
         "Rules:\n"
-        "- needs_retrieval=true if the question depends on domain-specific, private, or unknown info.\n"
-        "- needs_retrieval=false if it is purely conversational, generic, or can be answered without the corpus.\n\n"
-        f"User question:\n{user_text}\n"
+        "- needs_retrieval=true if the question depends on patent information.\n"
+        "- needs_retrieval=false if it is purely conversational, generic, or can be answered without the patent corpus.\n\n"
     )
 
     resp = nodes_model.invoke(prompt)
@@ -111,22 +108,38 @@ def retrieve_context(state: metadatastate, where_filter: Optional[Dict[str, Any]
         "contexts": chunks,
     }
 
-## Response Generation Node ##
+## Response Generation Node 1 ##
 
-generate_prompt = (
-    "You are a patent litigation assistant. \n"
-    "Use the following pieces of retrieved context to answer the question. "
-    "If you don't know the answer, just say that you don't know. "
-    "If no context is provided. Use your own knowledge to answer the question."
-    "Use three sentences maximum and keep the answer concise.\n"
-    "Question: {question} \n"
-    "Context: {context}"
+rusty_prompt = (
+    "You are a patent search and retrieval assistant.\n"
+    "Given the question: {question}"
+    "Use the following retrieved context to answer it:\n"
+    "{context}"
+    "Use three sentences maximum to respond and keep the answer concise.\n"
+    "If no context is provided. Respond that the provided context is not relevant to the question.\n"
 )
 
-def generate_answer(state: metadatastate)-> metadatastate:
+def rusty_answer(state: metadatastate)-> metadatastate:
     """Generate an answer."""
     question = state["cleaned_query"]
     context = state["joined_context"]
-    prompt = generate_prompt.format(question=question, context=context)
+    prompt = rusty_prompt.format(question=question, context=context)
+    response = nodes_model.invoke([{"role": "user", "content": prompt}])
+    return {"answer": [response.content]}
+
+
+## Response Generation Node 2 ##
+
+general_prompt = (
+    "You are a patent search and retrieval assistant.\n"
+    "Given the question: {question}"
+    "Respond the user consicely. If you do not know - just say that you do not know. \n"
+    "Use three sentences maximum to respond."
+)
+
+def general_answer(state: metadatastate)-> metadatastate:
+    """Generate an answer."""
+    question = state["cleaned_query"]
+    prompt = general_prompt.format(question=question)
     response = nodes_model.invoke([{"role": "user", "content": prompt}])
     return {"answer": [response.content]}
